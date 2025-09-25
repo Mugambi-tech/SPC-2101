d4e47d377820fe02f0ff3e5b4ee4ea84f33c29bd
Mugambi <mugambikimathidan14@gmail.com>
2025-09-25 13:41:32 +0300

    Add files via upload

---
+++ week 1 assignment.c
@@ -0,0 +1,25 @@
+/* Name: Dan Kimathi Mugambi
+Reg no:CT101/G/26458/25
+Descriptiom Week 1 Assignment 
+Date: 15/09
+*/
+#include<stdio.h>
+int main(){
+int height;
+int age;
+int phonenumber;
+int bankbalance;
+printf("1.Enter your height:");
+scanf("%d", & height);
+printf("Your height is %d.\n", height);
+printf("2.Enter your age: ");
+scanf("%d", & age);
+printf("Your age is %d.\n",age);
+printf("3.Enter your phone number:");
+scanf("%d", & phonenumber);
+printf("Your phone number is %d.\n", phonenumber);
+printf("4.Enter your bank balance: ");
+scanf("%d",& bankbalance);
+printf("your bank balance is %d.\n",bankbalance);
+return 0;
+}

c89db18422bc3d9f12dfd214ae406f0b31c3a301
Mugambi <mugambikimathidan14@gmail.com>
2025-09-25 12:29:51 +0300

    Add files via upload

---
+++ 8units.c
@@ -0,0 +1,22 @@
+/*Mugambi Kimathi Dan 
+ct101/g/26458/24
+my 8 units 
+*/
+#include<stdio.h>
+
+int main()
+{
+    printf("Hello world! \n");
+    printf("i am Dan kimathi \n");
+   //my eight units 
+   
+    printf("Introduction to programming \n");
+    printf("Introduction to computer architecture\n");
+    printf("Analogue electronics \n");
+    printf("Communication skills \n");
+    printf("Fundamentals of ict \n");
+    printf("Discrete mathematics \n");
+    printf("Mathematics for science \n");
+    printf("Physics");
+    return 0;
+}

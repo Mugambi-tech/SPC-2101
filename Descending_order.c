bd753e3265479c6ea6a689a405dd98be40bff8f9
Mugambi <mugambikimathidan14@gmail.com>
2025-10-09 13:34:12 +0300

    Add files via upload

---
+++ Descending_order.c
@@ -0,0 +1,19 @@
+/*
+Name: Dan Kimathi Mugambi 
+Reg no: CT101/G/26458/25
+Description: Descending_order
+09/10
+*/
+
+//pre proceesor directive
+#include <stdio.h>
+int main(){
+
+// for loop
+for(int i =100;i>=50;i--)
+{
+printf("%d \n",i);
+}
+return 0;
+}	
+		
---
+++ Password_system.c
@@ -0,0 +1,33 @@
+/*
+Name: Dan Kimathi Mugambi 
+Reg no: CT101/G/26458/25
+Description: Password system
+09/10
+*/
+
+//pre proccessor directive
+#include<stdio.h>
+
+//variable declaration
+int main(){
+
+int password;
+
+do{
+//Prints a message asking the user to enter their password.
+printf("\nEnter your password.\n");
+scanf("%d",& password);
+//Checks whether the entered password equals 1234
+if(password==1234){
+break;
+}
+printf("\nIncorrect password! Please try again. \n");
+}
+//Ends the loop. while(1) means “loop forever” until a break statement
+while (1);
+printf(" \nAccess Granted. \n");
+printf("Welcome.");
+
+	return 0;
+}
+		
